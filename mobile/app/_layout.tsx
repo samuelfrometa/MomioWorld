102d52a50961b89e2f2a749f6c260083dfaf845b
Samuel Frometa <sel.null.c30@gmail.com>
2026-02-04 12:17:07 +0100

    Splash terminado

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,21 +1,27 @@
 import { Stack } from "expo-router";
 import { useEffect, useState } from "react";
+import { useFonts } from "expo-font";
 import Splash from "./splash";
 
 export default function Layout() {
   const [ready, setReady] = useState(false);
 
+  const [fontsLoaded] = useFonts({
+    GravitasOne: require("../assets/fonts/Gravitas_One/GravitasOne-Regular.ttf"),
+    LeckerliOne: require("../assets/fonts/Leckerli_One/LeckerliOne-Regular.ttf"),
+  });
+
   useEffect(() => {
     const timer = setTimeout(() => {
       setReady(true);
-    }, 1000);
+    }, 5000);
 
     return () => clearTimeout(timer);
   }, []);
 
-  if (!ready) {
+  if (!ready || !fontsLoaded) {
     return <Splash />;
   }
 
-  return <Stack />;
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,44 +1,29 @@
 import { View, Text, StyleSheet, Pressable } from "react-native";
-import { router } from "expo-router";
+import { router, Stack } from "expo-router";
 
 export default function Home() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>MomioWorld</Text>
-
-      <Pressable
-        style={styles.button}
-        onPress={() => router.push("/tickets")}
-      >
-        <Text style={styles.buttonText}>Ir a Tickets 🎬</Text>
-      </Pressable>
-    </View>
+    <>
+      <View style={styles.container}>
+        <Text style={styles.title}>MUNDO</Text>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: "flex-start",
     alignItems: "center",
     backgroundColor: "#ffffff",
     padding: 24,
   },
   title: {
+    marginTop: "50",
     fontSize: 32,
     fontWeight: "700",
     marginBottom: 40,
     color: "#000",
-  },
-  button: {
-    backgroundColor: "#000",
-    paddingVertical: 14,
-    paddingHorizontal: 24,
-    borderRadius: 10,
-  },
-  buttonText: {
-    color: "#fff",
-    fontSize: 16,
-    fontWeight: "600",
   },
 });
